7f3fe34f3dd5c2150fad065334a1e9a0ebbd63a2
Luca Peric <lucaperic.lp@gmail.com>
2024-01-26 19:05:39 +0000

    Adding imports.js for easier livecoding

---
+++ hydra/imports.js
@@ -0,0 +1,9 @@
+await loadScript("https://hyper-hydra.glitch.me/hydra-text.js")
+await loadScript("https://hyper-hydra.glitch.me/hydra-tap.js")
+
+const myFont = new FontFace('PressStart', 'url(https://fonts.gstatic.com/s/pressstart2p/v15/e3t4euO8T-267oIAQAu6jDQyK3nVivNm4I81.woff2');
+await myFont.load();
+document.fonts.add(myFont);
+
+hydraText.font = "PressStart"
+hydraText.lineWidth = "2%"
